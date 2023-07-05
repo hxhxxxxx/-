a2d63ae0e1d02ef3556fc5f26cac885c34cbd414
黄锡浩 <107462276+hxhxxxxx@users.noreply.github.com>
2023-07-06 01:43:49 +0800

    Update 125.cpp

--- 125.cpp
+++ 125.cpp
@@ -1,6 +1,7 @@
 题目：验证回文串
 解题思路：主要是先处理空格，然后再把大小写转换一下，最后再反转一下字符串，比较二者是否相等即可
 不过按照这个思路的做法，在后面出现了超过内存限制，得考虑一些别的方法，这可能是跟我写了其他函数，用了新的储存有关，可以直接在s上进行处理
+关键核心：双指针
 class Solution {
 public:
     string trans(string s){
@@ -47,5 +48,26 @@ public:
         cout<<t<<' '<<t1<<endl;
         if(compare(t,t1))return true;
         else return false;
+    }
+};
+//由于上面代码用到了额外的空间，超出内存限制，所以为了避免空间的使用，直接在处理过的字符串上通过双指针直接比较
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        s=trans(del(s));
+        int siz=s.size();
+        int i=0,j=siz-1;
+        int flag=0;
+        while(i<j){
+            if(s[i]==s[j]){
+                i++;
+                j--;
+            }else{
+                flag=1;
+                break;
+            }
+        }
+        if(flag==1)return false;
+        else return true; 
     }
 };
