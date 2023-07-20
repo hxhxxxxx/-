9f25400368ed1b776ed4706a4f7e5869d2c350da
黄锡浩 <107462276+hxhxxxxx@users.noreply.github.com>
2023-07-20 17:34:42 +0800

    Create 139 单词拆分.cpp

---
+++ 139 单词拆分.cpp
@@ -0,0 +1,23 @@
+题目：单词拆分，给你一个字符串 s 和一个字符串列表 wordDict 作为字典。请你判断是否可以利用字典中出现的单词拼接出 s 。
+解题思路：先把单词放入哈希表中，然后对遍历字符串，从每个位置判断是否可以被字典里的词组成
+        不过这里用到了字符串的一些函数
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        auto worddicset=unordered_set<string>();
+        for(auto word: wordDict){
+            worddicset.insert(word);
+        }
+        auto dp=vector<bool>(s.size()+1);
+        dp[0]=true;
+        for(int i=1;i<=s.size();i++){
+            for(int j=0;j<i;j++){
+                if(dp[j]&& worddicset.find(s.substr(j,i-j))!=worddicset.end()){
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+        return dp[s.size()];
+    }
+};
