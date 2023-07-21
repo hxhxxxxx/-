ac5cc3445b646dd4425247a3725021f78b917169
黄锡浩 <107462276+hxhxxxxx@users.noreply.github.com>
2023-07-21 20:02:16 +0800

    Create 50 实现Pow(x,n).cpp

---
+++ 50 实现Pow(x,n).cpp
@@ -0,0 +1,37 @@
+题目：就是上面这个标题
+解题思路：对n的正负性进行判断，然后分类讨论
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(x==0)return 0.0;
+        double t;
+        if(n>0){
+                t=x;
+                for(int i=1;i<n;i++)t*=x;
+        }else{
+            t=1.0/x;
+            for(int i=1;i<-n;i++)t*=t;
+        }
+        return t;
+    }
+};
+上面的做法会遇到一个问题，因为对于分数相乘可能会导致误差比较大，所以应该先乘了之后再用1相除
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(x==0)return 0.0;
+        if(n==0)return 1.0;
+        if(n==1)return x;
+        double t;
+        if(n>0){
+                t=x;
+                for(int i=1;i<n;i++)t*=x;
+        }else{
+            t=x;
+            for(int i=1;i<-n;i++)t*=x;
+            t=1/t;
+        }
+        return t;
+    }
+};
+不过这个方法对于n比较小的情况是可以应对的，但是对于n比较大的时候会超时，所以需要别的方法
