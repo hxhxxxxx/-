abc3ea0abc2c161d310377b6b01008336ae5caa9
黄锡浩 <107462276+hxhxxxxx@users.noreply.github.com>
2024-06-04 14:20:50 +0800

    add
    添加一个题目

--- 74. 搜索二维矩阵.cpp
+++ 74. 搜索二维矩阵.cpp
@@ -1,11 +1,11 @@
-题目：
+/*题目：
 给你一个满足下述两条属性的 m x n 整数矩阵：
 每行中的整数从左到右按非递减顺序排列。
 每行的第一个整数大于前一行的最后一个整数。
 给你一个整数 target ，如果 target 在矩阵中，返回 true ；否则，返回 false 。
 
 解题思路：就先找到对应行，然后再在行里面找，优化可以在行里面用二分法来找
-
+*/
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
